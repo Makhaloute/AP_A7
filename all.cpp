be6b70e7f60ed3a71ee30986491ba32138d4dbb5
Makhaloute <sirmohammadmalaki@gmail.com>
2019-05-23 00:21:46 +0430

    initialize some more classes with thier basic functions and filds (for later implimentation) and complete part of comment struct

--- all.cpp
+++ all.cpp
@@ -8,17 +8,65 @@
 using namespace std;
 
 
+/*
+ the IFollow is a big class , that has a methods and these methods are defrent
+ so each child execute methods diffrently
+ */
 
 
-class User{
+
+
+class Notification{
+
+};
+class INotification{
+public:
+
+private:
+	
+};
+
+class IFollow{
 
 };
 
+class User{
+public:
+	bool isThisPublisher();
+	void addMoney(int amount);
+	int showNewNotifications();
+	int showAllNotifications();
+	
+
+	int addNotification(string nameOfUser, int userId, int notificationType, int filmId = 0);//unacceptable as is
+																							 //parse in some other functions
+private:
+	INotification* notifs;
+	IFollow* fowloings;
+	int moneyStock;
+	bool functionalType;
+	int id;
+	map <string,string> characteristics;
+};
+
+
+
+
+
 struct Comment{
+	comment(int _id, string _comment):id(_id),comment(_comment);
+	void addReply(string repy);
 	int id;
 	string comment;
 	vector <string> replyes;
 }
+void addReply(string repy){
+	if(reply != "")
+		replyes.push_back(reply);
+	else
+		cerr<<"there is an empty reply";
+}
+
 
 
 class Film{
@@ -36,7 +84,7 @@ private:
 	map <string,string> details;
 	vector<int> buyersId;
 	bool saleStatus;
-	double rate;
+	float rate;
 	int creatorId;
 };
 
@@ -50,22 +98,43 @@ public:
 	int replyToComment(int userId, int filmId, int commentId, string reply);
 	void showMyBoughtFilms(int userId);
 	void showFilmsIAdded(int userId);
+	int rateThisFilm(int filmId, float score);
 private:
 	vector <Film*> films;
 };
 
 
+
+
 class Imain(){
 public:
+	Imain(){films = new Ifilm();};
+	void signup(map <string,string> carectristics , bool userType);
+	int login(map <string,string> claims);
+
 
 private:
 
+	Ifilm * films;
+	User* currentUser;
+	vector<User*> users;
 };
 
 
 
 
 
-int main (){
-	;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main (){;}
