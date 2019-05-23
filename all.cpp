dfb3191e9a23b84fd749771bff14c16c023249ef
Makhaloute <sirmohammadmalaki@gmail.com>
2019-05-23 15:41:52 +0430

    impliment class notification

--- all.cpp
+++ all.cpp
@@ -16,19 +16,96 @@ using namespace std;
 
 
 
-class Notification{
 
+
+
+
+constexpr int PUBLISHER_FOLLOWED_NOTIF = 270;
+constexpr int PUBLISHER_BUOUGTYOURFILM_NOTIF = 250;
+constexpr int PUBLISHER_RATEDYOURFILM_NOTIF = 260;
+constexpr int PUBLISHER_COMMENTEDYOURFILM_NOTIF = 285;
+constexpr int USER_PUBLISERNEWFILM_NOTIF = 385;
+constexpr int USER_COMMENTREPLY_NOTIF = 485;
+
+
+class Notification{
+public:
+	Notification(string nameOfNotifMaker, int _notifMakerId, 
+		int _notificationType, int _filmId = 0, string _filmName = 0);
+
+	void printNotification();
+private:
+	string notifMakerName;
+	int notifMakerId;
+	int notificationType;
+	int filmId;
+	string filmName;
 };
+
+Notification::Notification(string nameOfNotifMaker, int _notifMakerId,
+					 int _notificationType, int _filmId = 0, string _filmName = 0){
+
+	notifMakerName = nameOfNotifMaker;
+	notifMakerId = _notifMakerId;
+	notificationType = _notificationType;
+	if (_notificationType == PUBLISHER_BUOUGTYOURFILM_NOTIF || _notificationType == PUBLISHER_RATEDYOURFILM_NOTIF || PUBLISHER_COMMENTEDYOURFILM_NOTIF){
+		filmId = _filmId;
+		filmName = _filmName;
+	}
+	else{
+		filmId = 0;
+		filmName = "";
+	}
+}
+
+void Notification::printNotification(){
+	if(notificationType == PUBLISHER_FOLLOWED_NOTIF)
+		cout << "User " << notifMakerName << " with id " << notifMakerId << " follow you." << endl;
+	if(notificationType == PUBLISHER_BUOUGTYOURFILM_NOTIF)
+		cout << "User " << notifMakerName << " with id " << notifMakerId 
+				<< " buy your film " << filmName << " with id " << filmId << endl;
+	if(notificationType == PUBLISHER_RATEDYOURFILM_NOTIF)
+		cout << "User " << notifMakerName << " with id " << notifMakerId 
+				<< " rate your film " << filmName << " with id " << filmId << endl;
+	if(notificationType == PUBLISHER_COMMENTEDYOURFILM_NOTIF)
+		cout << "User " << notifMakerName << " with id " << notifMakerId 
+				<< " rate your film " << filmName << " with id " << filmId << endl;
+	if(notificationType == USER_PUBLISERNEWFILM_NOTIF)
+		cout << "Publisher " << notifMakerName << " with id " 
+				<< NotifMakerId << " register new film."<<endl;
+	if(notificationType == USER_COMMENTREPLY_NOTIF)
+		cout << "Publisher " << notifMakerName << " with id " 
+				<< NotifMakerId << " reply to your comment."<<endl;
+}
+
+
+
+
+
+
+
 class INotification{
 public:
-
+	INotification();
+	void addNotification(string nameOfNotifMaker, int NotifMakerId,
+			 int notificationType, int filmId = 0, string _filmName = 0);
+	int showNewNotifications();
+	int showAllNotifications();
 private:
-	
+	vector <Notification*> unreadNotifications;//important note: if you pup then push imedieatly the order would fail
+											   // the way is a for loop (backward) ...  
+	vector <Notification*> readNotifications;
 };
 
-class IFollow{
 
-};
+
+
+
+
+
+
+
+
 
 class User{
 public:
@@ -36,17 +113,21 @@ public:
 	void addMoney(int amount);
 	int showNewNotifications();
 	int showAllNotifications();
-	
-
-	int addNotification(string nameOfUser, int userId, int notificationType, int filmId = 0);//unacceptable as is
-																							 //parse in some other functions
+	bool isHeSomeOneIFollow(int userId);
+	int addFollowr(int follwerId);
+	bool isMoneyEnogh(int price);
+	void subtractBuyingCost(int cost);
+	int addNotification(string nameOfUserWhoYourNotifiedFor,
+	 		int whoYourNotifiedForId, int notificationType, 
+	 			int filmId = 0, string _filmName = 0);//unacceptable as is
+													  //parse in some other functions
 private:
 	INotification* notifs;
-	IFollow* fowloings;
 	int moneyStock;
 	bool functionalType;
 	int id;
 	map <string,string> characteristics;
+	vector<int> follwersId;
 };
 
 
@@ -54,13 +135,15 @@ private:
 
 
 struct Comment{
-	comment(int _id, string _comment):id(_id),comment(_comment);
-	void addReply(string repy);
-	int id;
+	Comment(int _id, string _comment):id(_id),comment(_comment){}
+	void addReply(string reply);
+
 	string comment;
 	vector <string> replyes;
-}
-void addReply(string repy){
+	bool commentActiveness;
+	int id;
+};
+void Comment::addReply(string reply){
 	if(reply != "")
 		replyes.push_back(reply);
 	else
@@ -76,9 +159,11 @@ public:
 	int changeDetails(int userId, map<string,string> newDetails);
 	int addComment(string comment);
 	int replyToComment(int commentId);
-
-
+	void addRate(float newRate);
+	float getRate(){return rate;}
+	void addBuyer(int buyerId);
 	bool isUserTheCreator(int userId);
+	pair<int,float>tellPriceAndRate();
 private:
 	vector <Comment> comments;
 	map <string,string> details;
@@ -99,6 +184,8 @@ public:
 	void showMyBoughtFilms(int userId);
 	void showFilmsIAdded(int userId);
 	int rateThisFilm(int filmId, float score);
+	void buyFilm(int filmId, int buyerId);
+
 private:
 	vector <Film*> films;
 };
@@ -106,12 +193,32 @@ private:
 
 
 
-class Imain(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Imain{
 public:
 	Imain(){films = new Ifilm();};
 	void signup(map <string,string> carectristics , bool userType);
 	int login(map <string,string> claims);
-
+	int buyFilm(); // 
 
 private:
 
