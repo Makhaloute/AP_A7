6b02f3edb08e4a94d50c17633b64902f7d96f34b
Makhaloute <sirmohammadmalaki@gmail.com>
2019-06-07 21:29:16 +0430

    last changes

--- handlers.cpp
+++ handlers.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-Response *LoginHandler()::callback(Request *req , Imain mainInterface){
+Response *LoginHandler()::callback(Request *req , Imain* mainInterface){
   if(mainInterface->login(req->getQueryParam("name"),req->getQueryParam("password"))){
     Response *res = Response::redirect("/userhome");
     string id = stoi(mainInterface->returnId());
@@ -12,105 +12,44 @@ Response *LoginHandler()::callback(Request *req , Imain mainInterface){
   throw Server::Exception();
 } 
 
-
-Response *signupHandler()::callback(Request *req , Imain mainInterface){
+Response *signupHandler()::callback(Request *req , Imain* mainInterface){
   int type = 0;
   if (req->getQueryParam("type") == "publisher"){
     type = 1;
   }
-
-
- std::map<string, string> map = {{"age",req->getQueryParam("age")} }
-
-
-  if(mainInterface->signup(req->getQueryParam("name"), map <string,string> carectristics,Type)){
-
+  std::map<string, string> characteristics;
+  characteristics.insert ( std::pair<string,string>("name",req->getQueryParam("name")) );
+  characteristics.insert ( std::pair<string,string>("age",req->getQueryParam("age")) );
+  characteristics.insert ( std::pair<string,string>("password",req->getQueryParam("password")) );
+  characteristics.insert ( std::pair<string,string>("mail",req->getQueryParam("mail")) );
+  characteristics.insert ( std::pair<string,string>("type",req->getQueryParam("type")) );
+  if(mainInterface->signup(req->getQueryParam("name"),characteristics,Type)){
+    Response *res = Response::redirect("/userhome");
+    string id = stoi(mainInterface->returnId());
+    res->setSessionId(id);
+    return res;
   }
-
-
-
-  req->getQueryParam("password")
-req->getQueryParam("mail")
-req->getQueryParam("type")
-
-
-
-
-  req->getBodyParam("file_name")
-req->getBodyParam("file")
-
+  else{
+    throw Server::Exception();
+  }
 }
 
 
-
-
-Response *addfilmHandler()::callback(Request *req , Imain mainInterface){}
-Response *userHomeHandler()::callback(Request *req , Imain mainInterface){}
-Response *profileHandler()::callback(Request *req , Imain mainInterface){}
-Response *filmDetailHandler()::callback(Request *req , Imain mainInterface){}
-Response *logoutHandler()::callback(Request *req , Imain mainInterface){}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Response *RandomNumberHandler::callback(Request *req) {
-  Response *res = new Response;
-  res->setHeader("Content-Type", "text/html");
-  string body;
-  body += "<!DOCTYPE html>";
-  body += "<html>";
-  body += "<body style=\"text-align: center;\">";
-  body += "<h1>AP HTTP</h1>";
-  body += "<p>";
-  body += "a random number in [1, 10] is: ";
-  body += to_string(rand() % 10 + 1);
-  body += "</p>";
-  body += "<p>";
-  body += "SeddionId: ";
-  body += req->getSessionId();
-  body += "</p>";
-  body += "</body>";
-  body += "</html>";
-  res->setBody(body);
-  return res;
+Response *addfilmHandler()::callback(Request *req , Imain* mainInterface){
+  std::map<string, string> details;
+  details.insert ( std::pair<string,string>("name",req->getQueryParam("name")) );
+  details.insert ( std::pair<string,string>("price",req->getQueryParam("price")) );
+  details.insert ( std::pair<string,string>("director",req->getQueryParam("director")) );
+  details.insert ( std::pair<string,string>("length",req->getQueryParam("length")) );
+  details.insert ( std::pair<string,string>("year",req->getQueryParam("year")) );
+  details.insert ( std::pair<string,string>("summery",req->getQueryParam("summery")) );
+  if(mainInterface->addFilm(stoi(req->getSessionId()),details)){
+    Response *res = Response::redirect("/userhome");
+    return res;
+  }
+  else{
+    throw Server::Exception();
+  }
 }
 
-Response *LoginHandler::callback(Request *req) {
-  string username = req->getBodyParam("username");
-  string password = req->getBodyParam("password");
-  if (username == "root")
-    throw Server::Exception("Remote root access has been disabled.");
-  cout << "username: " << username << ",\tpassword: " << password << endl;
-  Response *res = Response::redirect("/rand");
-  res->setSessionId("SID");
-  return res;
-}
 
-Response *UploadHandler::callback(Request *req) {
-  string name = req->getBodyParam("file_name");
-  string file = req->getBodyParam("file");
-  cout << name << " (" << file.size() << "B):\n" << file << endl;
-  Response *res = Response::redirect("/");
-  return res;
-}
-
-LoginHandler::LoginHandler(string filePath) : TemplateHandler(filePath) {}
-
-map<string, string> LoginHandler::handle(Request *req) {
-  map<string, string> context;
-  string newName = "I am " + req->getQueryParam("name");
-  context["name"] = newName;
-  context["color"] = req->getQueryParam("color");
-  return context;
-}
--- newMain.cpp
+++ newMain.cpp
@@ -7,45 +7,29 @@ using namespace std;
 
 int main(int argc, char **argv) {
   try {
+    Imain* mainInterface;
     MyServer server(argc > 1 ? atoi(argv[1]) : 5000);
     server.setNotFoundErrPage("static/404.html");
  
     server.get("/login", new ShowPage("static/logincss.html"));
-    server.post("/login", new LoginHandler());
+    server.post("/login", new LoginHandler(mainInterface));
 
     server.get("/signup", new ShowPage("static/logincss.html"));
-    server.post("/signup", new signupHandler());
+    server.post("/signup", new signupHandler(mainInterface));
 
     server.get("/addfilm", new ShowPage("static/logincss.html"));
-    server.post("/addfilm", new addfilmHandler());
+    server.post("/addfilm", new addfilmHandler(mainInterface));
 
     server.get("/userhome", new ShowPage("static/logincss.html"));
-    server.post("/userhome", new userHomeHandler());
+    server.post("/userhome", new userHomeHandler(mainInterface));
 
 
     server.get("/profile", new ShowPage("static/logincss.html"));
-    server.post("/profile", new profileHandler());
+    server.post("/profile", new profileHandler(mainInterface));
 
     server.get("/filmdetail", new ShowPage("static/logincss.html"));
-    server.post("/filmdetail", new filmDetailHandler());
-
-
+    server.post("/filmdetail", new filmDetailHandler(mainInterface));
      server.get("/logout", new logoutHandler());
-
-
-
-   // server.get("/up", new ShowPage("static/upload_form.html"));
-    //server.post("/up", new UploadHandler());
-    
- 
-   
-   // server.get("/home.png", new ShowImage("static/home.png"));
-   
-  //  server.get("/", new ShowPage("static/home.html"));
-   
-   // server.get("/colors", new ColorHandler("template/colors.html"));
-   
-
     server.run();
   } catch (Server::Exception e) {
     cerr << e.getMessage() << endl;
